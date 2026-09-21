2f546139029e32140a9486a7d1a0ee4d6e4dd7fe
Anaïs BINET <anais.binet@fms-ea.com>
2026-09-21 15:22:03 +0200

    CREATE / IMPL : classe abstraire DAO

--- src/jdbc/dao/Dao.java
+++ src/jdbc/dao/Dao.java
@@ -1,5 +1,20 @@
 package jdbc.dao;
 
-public class Dao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class Dao {
+	
+	private static final String URL =
+	        "jdbc:mariadb://127.0.0.1:3306/Shop";
+
+	private static final String USER = "root";
+	private static final String PASSWORD = "";
+	
+	protected Connection getconnection() throws SQLException{
+		return DriverManager.getConnection(URL,USER,PASSWORD);
+		
+	}
 
 }
