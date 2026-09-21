ef9d24b5c0deaf516b3d8f4aedd163d9d739568b
Anaïs BINET <anais.binet@fms-ea.com>
2026-09-21 15:57:13 +0200

    IMPL : model article

--- src/jdbc/model/Article.java
+++ src/jdbc/model/Article.java
@@ -1,5 +1,57 @@
 package jdbc.model;
 
 public class Article {
+	
+	private int idArticle;
+	private String Description;
+	private String Brand;
+	private float UnitaryPrice;
+	
+	public Article(int idArticle, String Description, String Brand, float UnitatyPrice) {
+		this.idArticle = idArticle;
+		this.Description = Description;
+		this.Brand = Brand;
+		this.UnitaryPrice = UnitatyPrice;
+	}
+
+	public int getIdArticle() {
+		return idArticle;
+	}
+
+	public void setIdArticle(int idArticle) {
+		this.idArticle = idArticle;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	public String getBrand() {
+		return Brand;
+	}
+
+	public void setBrand(String brand) {
+		Brand = brand;
+	}
+
+	public float getUnitaryPrice() {
+		return UnitaryPrice;
+	}
+
+	public void setUnitaryPrice(float unitaryPrice) {
+		UnitaryPrice = unitaryPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [getIdArticle()=" + getIdArticle() + ", getDescription()=" + getDescription() + ", getBrand()="
+				+ getBrand() + ", getUnitaryPrice()=" + getUnitaryPrice() + "]";
+	}
+	
+	
 
 }
