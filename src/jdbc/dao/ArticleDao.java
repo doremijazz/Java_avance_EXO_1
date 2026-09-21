136fc07590c8ae3fc653d6c3789cde1831ce1d43
Anaïs BINET <anais.binet@fms-ea.com>
2026-09-21 15:41:29 +0200

    IMPL : methode read article en dao

--- src/jdbc/dao/ArticleDao.java
+++ src/jdbc/dao/ArticleDao.java
@@ -1,5 +1,10 @@
 package jdbc.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import jdbc.model.Article;
 
 public class ArticleDao extends Dao {
@@ -9,7 +14,19 @@ public class ArticleDao extends Dao {
 	}
 	
 	public Article read(int id) {
-		
+		String sql = "SELECT idArticle, Description, Brand, UnitatyPrice FROM t_articles WHERE idArticle = ?";
+		try(Connection connection = getconnection();
+				PreparedStatement statement = connection.prepareStatement(sql)){
+			statement.setInt(1, id);
+			
+			try(ResultSet result = statement.executeQuery()){
+				if (result.next()) {
+					return createArticleFromResult(result);
+				}
+			}
+		}catch (SQLException exception) {
+			System.err.println("Erreur lors d ela lecture de l'article : " + exception.getMessage());
+		}
 	}
 
 }
