ab85feb39af0817e23c7873bb3f4e6584fc9491c
Anaïs BINET <anais.binet@fms-ea.com>
2026-09-22 10:49:58 +0200

    TEST : Creer un article dans la db

--- src/jdbc/app/main.java
+++ src/jdbc/app/main.java
@@ -11,10 +11,13 @@ public class main {
 		ArticleDao Article_dao = new ArticleDao();
 		
 		//CREATE
+		Article article_1 = new Article("description", "brand", 3.50);
+		Boolean sucess = Article_dao.create(article_1); 
+		System.out.println("Création d'un article dans la db : " + sucess);
 		
 		//READ
-		Article article = Article_dao.read(1);
-		System.out.println("\n Lecture d'un article de la db : " + article);
+		Article article_2 = Article_dao.read(1);
+		System.out.println("\n Lecture d'un article de la db : " + article_2);
 		
 		//READ ALL
 		System.out.println("\n Liste des articles : ");
