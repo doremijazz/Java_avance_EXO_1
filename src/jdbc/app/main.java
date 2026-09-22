4e4c6d6fbcbcf3fe1891208fccb9181aceeca7e9
Anaïs BINET <anais.binet@fms-ea.com>
2026-09-22 09:46:52 +0200

    TEST : lecture de tous les article de la db dans l'app

--- src/jdbc/app/main.java
+++ src/jdbc/app/main.java
@@ -2,8 +2,27 @@ package jdbc.app;
 
 import java.sql.Connection;
 
+import jdbc.dao.Dao;
+import jdbc.model.Article;
+import jdbc.dao.ArticleDao;
+
 public class main {
 	public static void main (String[] args) throws Exception{
+		ArticleDao Article_dao = new ArticleDao();
+		
+		//CREATE
+		
+		//READ
+		
+		//READ ALL
+		System.out.println("\n Liste des articles : ");
+		for(Article currentArticle : Article_dao.reedAll()) {
+			System.out.println(currentArticle);
+		}
+		
+		//UPDATE
+		
+		//DELETE
 		
 	}
 }
